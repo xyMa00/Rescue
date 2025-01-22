0b5263012b68790290d79462c36af0f79cb12c28
mxy <1171205977@qq.com>
2025-01-22 09:18:02 +0800

    The original program.

--- setup.py
+++ setup.py
@@ -7,25 +7,25 @@ from setuptools import setup, find_packages
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
-setup(name='ressac',
-      version='1.1.2',
+setup(name='rescue',
+      version='1.0.0',
       packages=find_packages(),
-      description='Resnet based single-cell ATAC-seq clustering',
+      description='Resnet model employing scRNA-seq for characterizing cell composition by using expression of whole genome',
       long_description='',
 
 
       author='mxy',
       author_email='xyMa00@126.com',
-      url='https://github.com/xyMa00/bio_z7.git',
-      scripts=['Ressac.py'],
+      url='https://github.com/xyMa00/Rescue.git',
+      scripts=['Rescue.py'],
       install_requires=requirements,
-      python_requires='>3.7.0',
+      python_requires='>3.8.18',
       license='MIT',
 
       classifiers=[
           'Intended Audience :: Science/Research',
           'License :: OSI Approved :: MIT License',
-          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
           'Operating System :: MacOS :: MacOS X',
           'Operating System :: Microsoft :: Windows',
           'Operating System :: POSIX :: Linux',
